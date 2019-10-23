c18c58ac321a51176f0465700e161366a5bfb0cc
SHrud2k <shrud2k@gmail.com>
2019-10-23 15:04:19 +0300

    changed page initialization

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,11 +9,11 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        current_link = self.driver.current_url()
+        current_link = self.driver.current_url
         assert "login" in current_link, "Current page is not Login page"
 
     def should_be_login_form(self):
-        assert self.is_element_present(self, *LoginPageLocators.LOGIN_FORM), "Login form is not present"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present"
 
     def should_be_register_form(self):
-        assert self.is_element_present(self, *LoginPageLocators.REGISTER_FORM), "Register form is not present"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not present"
