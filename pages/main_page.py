34155d6fb53d70a68c01c7f98e46f8b1284e4703
SHrud2k <shrud2k@gmail.com>
2019-10-23 13:13:39 +0300

    Added invalid test; login check in MainPage

---
+++ pages/main_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        link = self.driver.find_element(By.CSS_SELECTOR, "#login_link")
+        link.click()
+
+    def should_be_login_link(self):
+        self.driver.find_element(By.CSS_SELECTOR, "#login_link_invalid")
